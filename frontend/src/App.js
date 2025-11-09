b4c2f925d35fbb475c695ee6fe95a3dd7ecacd39
George Dimas <georgedimas333@gmail.com>
2025-11-08 23:03:18 -0600

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={starfield} className="App-starfield" alt="starfield" />
-        <h1>Welcome to Soliel!</h1>
-        <h2>Soliel is the web application you need to monitor outer space's various weather phenomena</h2>
+        <h1>Welcome to Soleil!</h1>
+        <h2>Soleil is the web application you need to monitor outer space's various weather phenomena</h2>
+
+        <div className="space-weather-grid">
+        <div className="coronal-mass-ejection"></div>
+        <div className="solar-flares"></div>
+        <div className="geomagnetic-storms"></div>
+        <div className="interplanetary-shocks"></div>
+        </div>
+
         <a
           className="App-link"
           href="https://reactjs.org"
